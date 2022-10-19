9329d4303c5730a25dbca9e73da6a8c81164740f
Kemmie001 <oluwakemisola757@gmail.com>
2022-10-19 21:16:43 +0100

    Initial project scaffold
    Started this project using a basic scaffolding for local development using vue, typescript and vitest for unit testing. This scaffold is to set up instracture for the frontend web app for Toolhub records management
    
    Bug: T318921

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import {ref} from 'vue'
+// export default {
+//   data (){
+//     return{
+//       kemi: "hey",
+//     }
+//   }
+// }
+const kemi = ref("hey")
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
